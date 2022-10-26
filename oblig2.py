6f05e21935f89adfab5809a4875e30c223ad1fae
philipOlsen <78098113+philipOlsen@users.noreply.github.com>
2022-10-26 17:14:06 +0200

    Update oblig2.py

--- oblig2.py
+++ oblig2.py
@@ -15,10 +15,6 @@ def isLeapYear(intYear):
 def test_checks_if_var_intYear_is_leap_year_if_divisible_by_four_but_not_hundred_or_if_divisible_by_fourhundred():
     assert (isLeapYear(intYear))
 
-
-def test_checks_if_var_intYear_is_not_leap_year_by_returning_false_using_isLeapYear_function():
-    assert not (isLeapYear(intYear))
-
 #Function number two with test
 #Imports the calendar module and uses the .isLeap method that comes with it to check whether the result is True or False.
 year = 20
