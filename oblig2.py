9e5d660490366df326fb68f4deaa47b5132dd001
philipOlsen <78098113+philipOlsen@users.noreply.github.com>
2022-10-27 20:00:41 +0200

    Update oblig2.py

--- oblig2.py
+++ oblig2.py
@@ -1,8 +1,8 @@
 import pytest
 #Innlevering 2 - Philip Ringen Olsen // Software Engineering og Testing // Rettet
 
-#Function number one with test
-#By using an If test with leap year calculations the code checks if the year is a leap year by either returning True or False
+#Function number one with test.
+#By using an If test with leap year calculations the code checks if the year is a leap year or not.
 intYear = 2000
 def isLeapYear(intYear):
     if ((intYear % 4 == 0) and
@@ -12,9 +12,15 @@ def isLeapYear(intYear):
     else:
         return False
 
-def test_divisible_by_four_but_not_hundred():
+def test_divisible_by_four_but_not_hundred(intYear = 2000):
     assert (isLeapYear(intYear))
 
+def test_divisible_by_fourHundred(intYear = 400):
+    assert (isLeapYear(intYear))
+
+def test_year_not_within_acceptance_criteria(intYear = 1973):
+    assert not (isLeapYear(intYear))
+    
 #Function number two with test
 #Imports the calendar module and uses the .isLeap method that comes with it to check whether the result is True or False.
 year = 20
